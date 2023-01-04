4f38350dab4e36b79b4237d655688f0a08aff1b6
AgDelfino <agustin.delfino92@gmail.com>
2023-01-04 16:58:40 -0300

    mod(components/NavBar): modal register added

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,9 +4,11 @@ import { MdShoppingCart } from "react-icons/md";
 import Link from "next/link";
 import Categories from "./Categories";
 import { useState } from "react";
+import Register from "./Register";
 
 const NavBar = () => {
   const [activeCategoriesModal, setActiveCategoriesModal] = useState(false);
+  const [activeRegModal, setActiveRegModal] = useState(false)
 
   return (
     <header className="w-full">
@@ -62,6 +64,13 @@ const NavBar = () => {
           >
             Categorias
           </a>
+          <a
+            href="#"
+            className="xl:py-1 xl:px-2 rounded-lg hover:bg-fondo-100 transition-colors text-zinc-700 font-semibold"
+            onClick={() => setActiveRegModal((state) => !state)}
+          >
+            Register
+          </a>
           <button
             href="#"
             className="xl:py-1 xl:px-2 rounded-lg hover:bg-fondo-100 transition-colors"
@@ -74,6 +83,7 @@ const NavBar = () => {
         sourceArray={["Juguetes", "Jesucristo", "Panes", "Carros", "Ferraris"]}
         isActive={activeCategoriesModal}
       />
+      <Register registerArray={["Log In", "Sign Up"]} isActive={activeRegModal}/>
     </header>
   );
 };
--- pages/login.jsx
+++ pages/login.jsx
@@ -7,14 +7,14 @@ export default function Loading() {
       <div className="text-white flex flex-col items-center justify-center gap-8 p-8 max-w-lg mx-auto">
         <div className="flex flex-col gap-1 w-full">
           <h1 className="text-4xl font-medium text-fondo-200">Iniciar sesión</h1>
-          <p className="text-fondo-300 ">
+          <p className="text-fondo-400 ">
             Ingresa al sistema con tus credenciales
           </p>
         </div>
 
         <form className="flex flex-col gap-4">
           <div>
-            <label htmlFor="email" className="text-gray-200">
+            <label htmlFor="email" className="text-zinc-600">
               Correo electrónico *
             </label>
             <input
@@ -26,7 +26,7 @@ export default function Loading() {
             />
           </div>
           <div>
-            <label htmlFor="password" className="text-gray-200">
+            <label htmlFor="password" className="text-zinc-600">
               Contraseña *
             </label>
             <input
@@ -38,18 +38,18 @@ export default function Loading() {
             />
           </div>
           <div className="flex flex-col md:flex-row items-center justify-between gap-4 order-2 md:order-1">
-            <span className="text-gray-400">
+            <span className="text-gray-800">
               ¿No tienes cuenta?{' '}
               <Link
                 href="/signup"
-                className="text-fondo-200 hover:text-fondo-400 font-semibold  transition-colors"
+                className="text-fondo-500 hover:text-fondo-300 font-semibold  transition-colors"
               >
                 Registrate
               </Link>
             </span>
             <Link
               href="/#"
-              className="text-gray-400 hover:text-gray-500 transition-colors"
+              className="text-zinc-600 hover:text-fondo-300 transition-colors"
             >
               ¿Olvidaste tu contraseña?
             </Link>
@@ -58,13 +58,13 @@ export default function Loading() {
           <div className="w-full mt-4">
           <button
             type="button"
-            className="w-full flex items-center justify-center gap-2 border p-2 px-4 rounded-full bg-black hover:bg-white hover:text-black"
+            className="transition delay-125 ease-in-out w-full flex items-center justify-center gap-2 border p-2 px-4 rounded-full bg-black hover:bg-white hover:text-black"
           >
             <Image
               width={20}
               height={20}
               src='https://cdn-icons-png.flaticon.com/512/281/281764.png'
-              alt='Not found'
+              alt='Google'
             />
             <span className="ml-2">Ingresar con Google</span>
           </button>
@@ -73,7 +73,7 @@ export default function Loading() {
           <div className="mt-4 order-1 md:order-2">
             <button
               type="submit"
-              className="w-full font-semibold bg-fondo-300 p-2 rounded-full hover:bg-white hover:rounded-full hover:border hover:text-fondo-300 transition-colors"
+              className="border w-full font-semibold bg-fondo-300 p-2 rounded-full hover:bg-white hover:rounded-full hover:border hover:text-fondo-300 transition-colors"
             >
               Iniciar sesión
             </button>
