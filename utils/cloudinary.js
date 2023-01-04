b40e9f96b3c6b559bb5631c8092ff9d4bff635ca
Sojo506 <fabiansojowork@gmail.com>
2023-01-04 09:07:46 -0600

    add(utils/cloudinary): export default added

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,11 @@
 import cloudinary from "cloudinary";
 
-const cloud = cloudinary.v2
+const cloud = cloudinary.v2;
 
 cloud.config({
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.CLOUD_KEY, 
-  api_secret: process.env.CLOUD_SECRET 
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
+export default cloud;
