f8d16ea040b5ad126bc82348b11ee6dcc5d9453d
Sojo506 <fabiansojowork@gmail.com>
2023-01-04 09:03:45 -0600

    add(utils/cloudinary): I've added the utils folder in whici also i've added cloudinary.js

---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from "cloudinary";
+
+const cloud = cloudinary.v2
+
+cloud.config({
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_KEY, 
+  api_secret: process.env.CLOUD_SECRET 
+})
