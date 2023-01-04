3ab35676b2cd0f36dca5ed8f78df1cea853550b5
SamuelRnn <ricardo.reinn@outlook.com>
2023-01-03 21:30:16 -0500

    create&mod(/api): adjusted get/product and created delete/product routes

---
+++ pages/api/delete/product.js
@@ -0,0 +1,13 @@
+import { product } from "../../../prisma";
+
+export default async function handler(req, res) {
+  if (req.method !== "DELETE")
+    return res.status(400).send({ message: "Not found" });
+
+  const { productId } = req.query;
+  await product.delete({
+    where: {
+      id: productId,
+    },
+  });
+}
