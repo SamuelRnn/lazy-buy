d4b1e655f4288ad0ef86feb7adae26c9bd23557e
SamuelRnn <ricardo.reinn@outlook.com>
2023-01-05 03:41:00 -0500

    mod(/pages/404): autoredirect added

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import React from "react";
 
-const errorPage = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const Router = useRouter();
-  const handleClick = () => {
-    Router.push("/");
-  };
+const ErrorPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => router.push("/"), 5000);
+  }, []);
   return (
     <div className=" lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
       <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
@@ -17,16 +17,17 @@ const errorPage = () => {
               <h1 className="my-2 text-fondo-400 font-bold text-2xl">
                 Looks like you&apos;ve found the doorway to the great nothing
               </h1>
-              <p className="my-2 text-fondo-300">
+              {/* <p className="my-2 text-fondo-300">
                 Sorry about that! Please visit our hompage to get where you need
                 to go.
               </p>
               <button
-                onClick={handleClick}
+                onClick={() => router.push("/")}
                 className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-fondo-300 text-white ease-out duration-300 hover:-translate-x-2 "
               >
                 Back to Home
-              </button>
+              </button> */}
+              <p>Se te redigirá en un par de segundos...</p>
             </div>
           </div>
           <div>
@@ -53,4 +54,4 @@ const errorPage = () => {
   );
 };
 
-export default errorPage;
+export default ErrorPage;
