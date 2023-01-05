3c1e03bf96c8b715826276dc97a7bfb1f9b64f5a
Juampa1998 <Juampa_junin@hotmail.com>
2023-01-05 15:31:31 -0300

    modified(pages/login.jsx): controlled form

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,15 +1,16 @@
-import Link from "next/link";
-import Image from "next/image";
-import AnimatedLogo from "../components/AnimatedLogo";
-import Layout from "../components/layout";
-import { signIn } from "next-auth/react";
+import Link from 'next/link'
+import Image from 'next/image'
+import AnimatedLogo from '../components/AnimatedLogo'
+import Layout from '../components/layout'
+import { signIn } from 'next-auth/react'
+import { Formik } from 'formik'
 
-const callbackUrl = process.env.BASE_URL || "http://localhost:3000";
+// const callbackUrl = process.env.BASE_URL || 'http://localhost:3000'
 
 export default function Loading() {
-  async function googleSignin() {
-    await signIn("google", { callbackUrl });
-  }
+  // async function googleSignin() {
+  //   await signIn('google', { callbackUrl })
+  
   return (
     <Layout noLayout={true}>
       <div className="min-h-screen bg-[#ffffff] grid grid-cols-1 lg:grid-cols-2">
@@ -23,74 +24,109 @@ export default function Loading() {
             </p>
           </div>
 
-          <form className="flex flex-col gap-4">
-            <div>
-              <label htmlFor="email" className="text-zinc-600">
-                Correo electrónico *
-              </label>
-              <input
-                type="email"
-                id="email"
-                autoComplete="off"
-                className="w-full py-2 px-4 bg-transparent border rounded-full mt-2 outline-none focus:border-fondo-400 text-black"
-                placeholder="Ingresa tu correo electrónico"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="text-zinc-600">
-                Contraseña *
-              </label>
-              <input
-                type="password"
-                id="password"
-                autoComplete="off"
-                className="w-full py-2 px-4 bg-transparent border rounded-full mt-2 outline-none focus:border-fondo-400 text-black"
-                placeholder="Ingresa tu contraseña"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row items-center justify-between gap-4 order-2 md:order-1">
-              <span className="text-gray-800">
-                ¿No tienes cuenta?{" "}
-                <Link
-                  href="/signup"
-                  className="text-fondo-500 hover:text-fondo-300 font-semibold  transition-colors"
-                >
-                  Registrate
-                </Link>
-              </span>
-              <Link
-                href="/#"
-                className="text-zinc-600 hover:text-fondo-300 transition-colors"
-              >
-                ¿Olvidaste tu contraseña?
-              </Link>
-            </div>
-
-            <div className="w-full mt-4">
-              <button
-                onClick={googleSignin}
-                type="button"
-                className="transition delay-125 ease-in-out w-full flex items-center justify-center gap-2 border p-2 px-4 rounded-full bg-black hover:bg-white hover:text-black"
-              >
-                <Image
-                  width={20}
-                  height={20}
-                  src="https://cdn-icons-png.flaticon.com/512/281/281764.png"
-                  alt="Google"
-                />
-                <span className="ml-2">Ingresar con Google</span>
-              </button>
-            </div>
-
-            <div className="mt-4 order-1 md:order-2">
-              <button
-                type="submit"
-                className="border w-full font-semibold bg-fondo-300 p-2 rounded-full hover:bg-white hover:rounded-full hover:border hover:text-fondo-300 transition-colors"
-              >
-                Iniciar sesión
-              </button>
-            </div>
-          </form>
+          <Formik
+            initialValues={{
+              email: '',
+              password: ''
+            }}
+            validate={(valores) =>{
+              let errors = {}
+              if(!valores.email){
+                errors.email = 'Por favor ingresa un email'
+              }
+              if(!valores.password){
+                errors.password = 'Por favor ingresa un password'
+              }
+              return errors
+            }}
+            onSubmit={(valores) =>{
+              // console.log(valores) 
+              console.log('Formulario enviado')
+            }}
+          >
+            {( {errors, values, handleSubmit, handleChange, handleBlur }) => (
+                 <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                  {/* {console.log(props)} propiedades del formik */}
+                  {console.log(errors)}
+                 {/* Email */}
+                 <div>
+                   <label htmlFor="email" className="text-zinc-600">
+                     Correo electrónico *
+                   </label>
+                   <input
+                     type="email"
+                     id="email"
+                     autoComplete="off"
+                     className="w-full py-2 px-4 bg-transparent border rounded-full mt-2 outline-none focus:border-fondo-400 text-black"
+                     placeholder="Ingresa tu correo electrónico"
+                     value={values.email}
+                     onChange={handleChange}
+                    //  onBlur={handleBlur}
+                   />
+                   {errors.email && <div className='text-red-600 mt-2 pl-5'>{errors.email}</div>}
+                 </div>
+                 {/* Contraseña */}
+                 <div>
+                   <label htmlFor="password" className="text-zinc-600">
+                     Contraseña *
+                   </label>
+                   <input
+                     type="password"
+                     id="password"
+                     autoComplete="off"
+                     className="w-full py-2 px-4 bg-transparent border rounded-full mt-2 outline-none focus:border-fondo-400 text-black"
+                     placeholder="Ingresa tu contraseña"
+                     value={values.password}
+                     onChange={handleChange}
+                    //  onBlur={handleBlur}
+                   />
+                   {errors.password && <div className='text-red-600 mt-2 pl-5'>{errors.password}</div>}
+                 </div>
+                 <div className="flex flex-col md:flex-row items-center justify-between gap-4 order-2 md:order-1">
+                   <span className="text-gray-800">
+                     ¿No tienes cuenta?{' '}
+                     <Link
+                       href="/signup"
+                       className="text-fondo-500 hover:text-fondo-300 font-semibold  transition-colors"
+                     >
+                       Registrate
+                     </Link>
+                   </span>
+                   <Link
+                     href="/#"
+                     className="text-zinc-600 hover:text-fondo-300 transition-colors"
+                   >
+                     ¿Olvidaste tu contraseña?
+                   </Link>
+                 </div>
+   
+                 <div className="w-full mt-4">
+                   <button
+                    //  onClick={googleSignin}
+                     type="button"
+                     className="transition delay-125 ease-in-out w-full flex items-center justify-center gap-2 border p-2 px-4 rounded-full bg-black hover:bg-white hover:text-black"
+                   >
+                     <Image
+                       width={20}
+                       height={20}
+                       src="https://cdn-icons-png.flaticon.com/512/281/281764.png"
+                       alt="Google"
+                     />
+                     <span className="ml-2">Ingresar con Google</span>
+                   </button>
+                 </div>
+   
+                 <div className="mt-4 order-1 md:order-2">
+                   <button
+                     type="submit"
+                     className="border w-full font-semibold bg-fondo-300 p-2 rounded-full hover:bg-white hover:rounded-full hover:border hover:text-fondo-300 transition-colors"
+                   >
+                     Iniciar sesión
+                   </button>
+                 </div>
+               </form>
+            )}
+          </Formik>
         </div>
         {/* AnimatedLogo */}
         <div className="hidden lg:flex items-center justify-center">
@@ -98,5 +134,5 @@ export default function Loading() {
         </div>
       </div>
     </Layout>
-  );
+  )
 }
