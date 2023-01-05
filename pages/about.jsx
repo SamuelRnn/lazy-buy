553623c68623dccf7c64583127d2af8c80e97a2c
Bird97 <juamacx@gmail.com>
2023-01-05 12:10:24 -0500

    about move img to left

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,6 +8,14 @@ const About1 = () => {
         <NavBar/>         
         <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
             <div className="flex flex-col lg:flex-row justify-between gap-8">
+            <div className="w-full lg:w-8/12 ">
+                <Image
+                    src="https://i.ibb.co/FhgPJt8/Rectangle-116.png"
+                    alt="A group of People"                  
+                    height={315}
+                    width={733}
+                />
+                 </div>
                 <div className="w-full lg:w-5/12 flex flex-col justify-center">
                 <p className="font-normal text-base leading-6 text-zinc-700 "><h1 className="text-3xl lg:text-4xl font-bold leading-9 text-fondo-400 pb-4"><span className="text-fondo-500 uppercase">Lazzy Buy</span> is carried out by passionate developers.</h1>
                 Lazzy Buy is a project made by a group of 7 students from Henry Bootcamp The project is based on creating a full functional website. In
@@ -16,15 +24,7 @@ const About1 = () => {
                 this project we were working with the following technologies:
                 Next.JS, JSX, Tailwind & Prisma.</p>
                 </div>
-                <div className="w-full lg:w-8/12 ">
-                <Image
-                    src="https://i.ibb.co/FhgPJt8/Rectangle-116.png"
-                    alt="A group of People"                  
-                    height={315}
-                    width={733}
-                />
-
-                 </div>
+                
             </div>
 
             <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
