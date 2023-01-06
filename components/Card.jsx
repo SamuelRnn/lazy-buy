c9fd7aa8078f0b3b1b7b79dc20631c59bad32f7f
SamuelRnn <ricardo.reinn@outlook.com>
2023-01-06 00:55:49 -0500

    added(/components & pages): la vida

--- components/Card.jsx
+++ components/Card.jsx
@@ -7,7 +7,7 @@ const mock_product = {
   slug: "43-renault-modelo-verano-rtx-kojimo",
   price: 1000.0,
   company: { name: "Crocs & Sneakers SA" },
-  image: "https://bicialtea.labici.net/images/bicialtea.png",
+  mainImage: { url: "https://bicialtea.labici.net/images/bicialtea.png" },
 };
 //recibe 2 styles: "card" "wider"
 const Card = ({ product = mock_product, style = "card" }) => {
@@ -16,21 +16,22 @@ const Card = ({ product = mock_product, style = "card" }) => {
       {style === "card" && (
         <div className="bg-white max-w-sm mx-auto rounded-lg shadow-xl card_width hover:scale-95 transition-transform">
           <div className="py-4 px-6 flex flex-col gap-2">
-            <h3 className="text-2xl font-bold text-gray-700">{product.name}</h3>
-            <p className="text-gray-500">{product.company.name}</p>
+            <h3 className="text-xl font-bold text-gray-700">{product.name}</h3>
+            <p className="text-gray-500 text-sm">{product.company.name}</p>
           </div>
           <div>
             <Image
-              src={product.image}
+              src={product.mainImage.url}
               alt="Adidas"
               className="h-44 mx-auto object-cover"
               height={44}
               width={150}
-              object-fit="cover"
             />
           </div>
           <div className="py-4 px-6 flex items-center justify-between bg-pry-600 rounded-bl-lg rounded-br-lg">
-            <span className="text-white font-bold">$1,500.00</span>
+            <span className="text-white font-bold">
+              ${product.price.toFixed(2)}
+            </span>
             <button className="text-gray-200 hover:text-white transition-colors font-semibold outline-none">
               Agregar al carrito
             </button>
@@ -39,16 +40,18 @@ const Card = ({ product = mock_product, style = "card" }) => {
       )}
       {/* wider style for Card */}
       {style === "wider" && (
-        <div className="bg-white w-[800px] h-[200px] rounded-lg shadow-xl hover:scale-110 transition-transform">
+        <div className="bg-white h-[200px] w-[800px] rounded-lg shadow-xl hover:scale-110 transition-transform">
           <div className="p-6 flex">
-            <Image
-              src={product.image}
-              alt="Adidas"
-              height={152}
-              width={152}
-              object-fit="cover"
-            />
-            <div className="w-full px-8 bg-zinc-100 rounded pt-4">
+            <div className="mr-6 h-[152px] w-[152px]">
+              <Image
+                src={product.mainImage.url}
+                alt="Adidas"
+                height={152}
+                width={152}
+                className="h-[152px] w-[152px] bg-white object-cover brightness-105 mix-blend-multiply"
+              />
+            </div>
+            <div className="w-full px-6 bg-zinc-100 rounded pt-4">
               <h3 className="font-bold text-xl text-gray-700">
                 {product.name}
               </h3>
@@ -56,7 +59,7 @@ const Card = ({ product = mock_product, style = "card" }) => {
 
               <div className="mt-6">
                 <p className="font-mono font-bold text-2xl text-fondo-300">
-                  ${product.price}
+                  ${product.price.toFixed(2)}
                 </p>
               </div>
             </div>
---
+++ components/FiltersModal/Filters.jsx
@@ -0,0 +1,5 @@
+const Filters = () => {
+  return <div className="p-4">Filters</div>;
+};
+//logica de los filtros y RTK query
+export default Filters;
