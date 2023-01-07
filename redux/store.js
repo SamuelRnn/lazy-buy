1520b5a3fe52397e6054510667d7154872a24ee5
SamuelRnn <ricardo.reinn@outlook.com>
2023-01-07 05:20:46 -0500

    config(/): added redux store with persist

--- redux/store.js
+++ redux/store.js
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import { productApi } from "./productsApi";
 import { cartSlice } from "./cartSlice";
+import { productApi } from "./productsApi";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
+  [productApi.reducerPath]: productApi.reducer,
+  cart: cartSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    [productApi.reducerPath]: productApi.reducer,
-    cart: cartSlice.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productApi.middleware),
 });
