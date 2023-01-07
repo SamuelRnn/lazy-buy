06ea6453b91eef1e26febe0d64bb64f1e6d51f14
SamuelRnn <ricardo.reinn@outlook.com>
2023-01-06 23:54:07 -0500

    added(/redux): added redux store for cart

---
+++ redux/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  subtotal: 0,
+};
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    setCartItem: (state, action) => {},
+    cutCartItem: (state, action) => {},
+  },
+});
+export const { setCartItem, cutCartItem } = cartSlice.actions;
+//custom selector function => useSelector(getCart) instead of useSelector(state => state.counter)
+export default getCart = (state) => state.counter;
--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { productApi } from "./productsApi";
+import { cartSlice } from "./cartSlice";
+
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
+    cart: cartSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productApi.middleware),
