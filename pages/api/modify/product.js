00627864fa4b7335a36d5c366b25e059baade0d9
Sojo506 <fabiansojowork@gmail.com>
2023-01-09 20:33:24 -0600

    add(dashboard/product): Now you can delete (logic eraser) products. Route modfy product added. Route delete product modified

--- pages/api/modify/product.js
+++ pages/api/modify/product.js
@@ -1,24 +1,24 @@
 import { product } from "../../../prisma";
 
 export default async function handler(req, res) {
-  if (req.method !== "DELETE")
+  if (req.method !== "PUT")
     return res.status(400).send({ message: "Not found" });
 
   const { productId } = req.query;
-  
+
   const setActive = await product.findFirst({
     where: {
-      id: parseInt(productId)
-    }
-  })
+      id: parseInt(productId),
+    },
+  });
 
   await product.update({
     where: {
-      id: parseInt(productId)
+      id: parseInt(productId),
     },
     data: {
-      isActive: !setActive.isActive
-    }
-  })
-  res.status(202).json({ ok: true, message: "Product deleted successfully!" });
+      isActive: !setActive.isActive,
+    },
+  });
+  res.status(202).json({ ok: true, message: "Product modified successfully!" });
 }
