3d8464f41fe112218fc99f19544204cd12c62070
Sojo506 <fabiansojowork@gmail.com>
2023-01-09 19:07:46 -0600

    test(dashboard/performance): testing chart js

--- pages/dashboard/performance.jsx
+++ pages/dashboard/performance.jsx
@@ -1,10 +1,37 @@
 import DashboardLayout from "../../components/DashBoard/Layout";
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
 const Performance = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Bill",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [1, 10, 5, 2, 13, 21, 11],
+      },
+    ],
+  };
+  const options = {
+    mainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    legend: {
+      labels: {
+        fontSize: 22,
+      },
+    },
+  };
+
   return (
     <DashboardLayout>
       <div>
-        <h1>Performance</h1>
+        <Bar data={data} options={options} height={200} />
       </div>
     </DashboardLayout>
   );
