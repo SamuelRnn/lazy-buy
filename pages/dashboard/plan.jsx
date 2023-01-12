4a789afab5ab06488fdb5099887f588b77fe1083
AgDelfino <agustin.delfino92@gmail.com>
2023-01-12 18:50:30 -0300

    mod(pages/dashboard/plan.jsx): modified styles

--- pages/dashboard/plan.jsx
+++ pages/dashboard/plan.jsx
@@ -15,18 +15,23 @@ const Plan = ({ company: { email } }) => {
               Designed specially for your business
             </h2>
             <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
-              Here at Lazy-Buy we focus on your growth where technology, innovation,
-              and capital can unlock long-term value and drive to your next level
+              Here at Lazy-Buy we focus on your growth where technology,
+              innovation, and capital can unlock long-term value and drive to
+              your next level
             </p>
           </div>
           <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-            <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow">
-              <h3 class="mb-4 text-2xl font-semibold">Basic</h3>
-              <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                Best option for personal use & for your next project.
-              </p>
+            <div class="flex flex-col justify-between p-6 mx-auto max-w-lg text-center text-gray-900 bg-zinc-100 rounded-lg border border-gray-100 shadow">
+              <div>
+                <h3 class="mb-4 text-2xl font-semibold">Basic</h3>
+                <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                  Start selling on our marketplace with no financial commitment
+                  by signing up for our Free plan, offering basic features for
+                  new and casual sellers.
+                </p>
+              </div>
               <div class="flex justify-center items-baseline my-8">
-                <span class="mr-2 text-5xl font-extrabold">$29</span>
+                <span class="mr-2 text-5xl font-extrabold">$0</span>
                 <span class="text-gray-500 dark:text-gray-400">/month</span>
               </div>
 
@@ -86,10 +91,12 @@ const Plan = ({ company: { email } }) => {
                 Get started
               </a>
             </div>
-            <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-fondo-100 rounded-lg border border-fondo-100 shadow">
+            <div class="flex flex-col justify-between p-6 mx-auto max-w-lg text-center text-gray-900 bg-gray-500  rounded-lg border border-gray-500 shadow">
               <h3 class="mb-4 text-white text-2xl font-semibold">Standard</h3>
               <p class="font-light text-fondo-50 sm:text-lg">
-                Best option for personal use & for your next project.
+                Elevate your sales to new heights with our Standard plan,
+                everything you need to succeed as a top seller on our
+                marketplace.
               </p>
               <div class="flex justify-center items-baseline my-8">
                 <span class="mr-2 text-white text-5xl font-extrabold">$69</span>
@@ -97,7 +104,7 @@ const Plan = ({ company: { email } }) => {
               </div>
 
               <ul role="list" class="mb-8 space-y-4 text-left text-white">
-              <li class="flex items-center space-x-3">
+                <li class="flex items-center space-x-3">
                   <svg
                     class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
                     fill="currentColor"
@@ -152,18 +159,22 @@ const Plan = ({ company: { email } }) => {
                 Get started
               </a>
             </div>
-            <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow">
-              <h3 class="mb-4 text-2xl font-semibold">Premmium</h3>
-              <p class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                Best option for personal use & for your next project.
-              </p>
+            <div class="flex flex-col justify-between p-6 mx-auto max-w-lg text-center text-white bg-[#e5c8a1] rounded-lg border border-gray-100 shadow">
+                <div>
+                  <h3 class="mb-4 text-2xl font-semibold">Premmium</h3>
+                  <p class="font-light text-white sm:text-lg dark:text-gray-400">
+                    Unlock the full potential of our marketplace and take your
+                    sales to the next level with our Premium plan, featuring
+                    exclusive features.
+                  </p>
+                </div>
               <div class="flex justify-center items-baseline my-8">
                 <span class="mr-2 text-5xl font-extrabold">$99</span>
-                <span class="text-gray-500 dark:text-gray-400">/month</span>
+                <span class="text-white">/month</span>
               </div>
 
               <ul role="list" class="mb-8 space-y-4 text-left">
-              <li class="flex items-center space-x-3">
+                <li class="flex items-center space-x-3">
                   <svg
                     class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
                     fill="currentColor"
@@ -191,7 +202,7 @@ const Plan = ({ company: { email } }) => {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  <span>Products visualized: +99 products</span>
+                  <span>Products visualized: Unlimited</span>
                 </li>
                 <li class="flex items-center space-x-3">
                   <svg
@@ -207,7 +218,7 @@ const Plan = ({ company: { email } }) => {
                     ></path>
                   </svg>
                   <span>
-                    Store size: <span class="font-semibold">+99 products</span>
+                    Store size: <span class="font-semibold">Unlimited</span>
                   </span>
                 </li>
               </ul>
