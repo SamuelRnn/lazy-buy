99c74a34e736ad7060ae10fa0b0ef6f4a4744c7b
Bravoezz <joelmrd1234@gmail.com>
2023-01-13 00:16:29 -0500

    refactoring(pages/paymentIs)

--- pages/paymentIs.jsx
+++ pages/paymentIs.jsx
@@ -4,8 +4,18 @@ import { motion } from "framer-motion";
 const paymentIs = () => {
   const [btn, setBtn] = useState(false);
 
+  let arr = [
+    "md:block hidden fixed h-full -left-20 -bottom-20",
+    "md:block hidden fixed h-full -left-20 -top-28",
+    "md:block hidden fixed h-full -right-20 -bottom-20",
+    "md:block hidden fixed h-full -right-20 -top-28",
+    "md:block hidden fixed h-full  ",
+  ];
+
   useEffect(() => {
-    setTimeout(() => {setBtn(true)}, 2000);
+    setTimeout(() => {
+      setBtn(true);
+    }, 2000);
   }, []);
 
   return (
@@ -19,66 +29,36 @@ const paymentIs = () => {
           />
           <h1 className="text-fondo-1000 text-4xl">Payment Successful!!</h1>
           <h2>algunas cosas</h2>
-          {btn && <motion.button
-          initial={{scale:0,opacity:0}}
-          animate={{ scale: 1,opacity:1 }}
-            type="button"
-            class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
-          >
-            Back to Site
-          </motion.button>}
+          {btn && (
+            <motion.button
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              type="button"
+              class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+            >
+              Back to Site
+            </motion.button>
+          )}
           {!btn && <div className="h-11"> </div>}
         </div>
       </div>
       <div className="w-3/5 z-40">
         <img
-        
           className="-scale-x-100"
           src="https://images.vexels.com/media/users/3/210362/isolated/preview/44acab728548ca17f29211eeda51f2e3-hombre-reclinado-con-caracter-de-telefono-celular.png"
           alt="men"
         />
       </div>
-
-      <motion.img
-      initial={{scale:0,opacity:0}}
-      animate={{ scale: 1,opacity:[1,0.5,0,0.5,1] }}
-      transition={{ delay:0.2, type: "spring", stiffness: 1000 }}
-        className="md:block hidden fixed h-full -left-20 -bottom-20"
-        src="https://png.pngtree.com/png-clipart/20211128/original/pngtree-confetti-vector-png-for-festival-free-colorful-tinsel-explosion-on-transparent-png-image_6953842.png"
-        alt="img"
-      />
-      <motion.img
-      initial={{scale:0,opacity:0}}
-      animate={{ scale: 1,opacity:[1,0.5,0,0.5,1] }}
-      transition={{ delay:0.2, type: "spring", stiffness: 1000 }}
-        className="md:block hidden fixed h-full -left-20 -top-28    "
-        src="https://png.pngtree.com/png-clipart/20211128/original/pngtree-confetti-vector-png-for-festival-free-colorful-tinsel-explosion-on-transparent-png-image_6953842.png"
-        alt="img"
-      />
-      <motion.img
-      initial={{scale:0,opacity:0}}
-      animate={{ scale: 1,opacity:[1,0.5,0,0.5,1] }}
-      transition={{ delay:0.2, type: "spring", stiffness: 1000 }}
-        className="md:block hidden fixed h-full -right-20 -bottom-20"
-        src="https://png.pngtree.com/png-clipart/20211128/original/pngtree-confetti-vector-png-for-festival-free-colorful-tinsel-explosion-on-transparent-png-image_6953842.png"
-        alt="img"
-      />
-      <motion.img
-      initial={{scale:0,opacity:0}}
-      animate={{ scale: 1,opacity:[1,0.5,0,0.5,1] }}
-      transition={{ delay:0.2, type: "spring", stiffness: 1000 }}
-        className="md:block hidden fixed h-full -right-20 -top-28    "
-        src="https://png.pngtree.com/png-clipart/20211128/original/pngtree-confetti-vector-png-for-festival-free-colorful-tinsel-explosion-on-transparent-png-image_6953842.png"
-        alt="img"
-      />
-      <motion.img
-      initial={{scale:0,opacity:0}}
-      animate={{ scale: 1,opacity:[1,0.5,0,0.5,1] }}
-      transition={{ delay:0.2, type: "spring", stiffness: 1000 }}
-        className="md:block hidden fixed h-full  "
-        src="https://png.pngtree.com/png-clipart/20211128/original/pngtree-confetti-vector-png-for-festival-free-colorful-tinsel-explosion-on-transparent-png-image_6953842.png"
-        alt="img"
-      />
+      {arr?.map((str) => (
+        <motion.img
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: [1, 0.5, 0, 0.5, 1] }}
+          transition={{ delay: 0.2, type: "spring", stiffness: 1000 }}
+          className={str}
+          src="https://png.pngtree.com/png-clipart/20211128/original/pngtree-confetti-vector-png-for-festival-free-colorful-tinsel-explosion-on-transparent-png-image_6953842.png"
+          alt="img"
+        />
+      ))}
     </div>
   );
 };
