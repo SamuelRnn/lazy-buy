7c9201016309d916e0eafac4039c7502b48efcab
Sojo506 <fabiansojowork@gmail.com>
2023-01-15 18:39:34 -0600

    feat(pages/products): add to cart from detail card

--- pages/products/[slug].jsx
+++ pages/products/[slug].jsx
@@ -2,9 +2,39 @@ import Layout from "../../components/Layout";
 import Image from "next/image";
 import { PrismaClient } from "@prisma/client";
 import { useRouter } from "next/router";
+import { useSelector, useDispatch } from "react-redux";
+import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import {
+  getCart,
+  setCartItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} from "../../redux/cartSlice";
+import { toast } from "react-hot-toast";
 
 const Detail = ({ product }) => {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = useState(1);
+  const accountType = useSelector((state) => state.account?.session.type);
+  const cart = useSelector(getCart);
+
+  const addItemToCart = () => {
+    if (accountType === "company") {
+      return toast.error("Company accounts can't buy or have a cart!");
+    }
+    const itemAlreadyExist = cart.find((item) => item.id === product.id);
+    if (itemAlreadyExist) {
+      return toast.error(`"${product.name}" is already in your cart!`);
+    }
+
+    dispatch(setCartItem({ ...product, quantity }));
+    setQuantity(1);
+    toast.success(`"${product.name}" added sucessfully to your cart :D`, {
+      duration: 4000,
+    });
+  };
 
   return (
     <Layout title={"Lazy Buy | " + product.name}>
@@ -119,7 +149,7 @@ const Detail = ({ product }) => {
                 </div> */}
               </details>
 
-              <form className="mt-8">
+              <div className="mt-8">
                 {/* //add props extra to model  */}
                 {/* <fieldset>
                   <legend className="mb-1 text-sm font-medium">Color</legend>
@@ -151,7 +181,7 @@ const Detail = ({ product }) => {
                   <legend className="mb-1 text-sm font-medium">Size</legend>
                 </fieldset> */}
 
-                <div className="flex mt-8">
+                <div className="flex mt-8 flex-col gap-4">
                   <div>
                     {/*  <input
                       type="number"
@@ -159,12 +189,40 @@ const Detail = ({ product }) => {
                       className="w-12 rounded border-gray-200 py-3 text-center text-xs [-moz-appearance:_textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
                     /> */}
                   </div>
+                  <div className="flex justify-between items-center mb-5 px-5">
+                    <button
+                      className=""
+                      onClick={() => {
+                        if (quantity === 1)
+                          return toast.error(
+                            "Select at least 1 item, if you want to remove the item click on the trash can"
+                          );
 
-                  <button className="block px-5 py-3 ml-3 text-xs font-medium text-white bg-fondo-300 rounded hover:bg-fondo-500 active:scale-75 transition-all ease-out">
+                        setQuantity(quantity - 1);
+                      }}
+                    >
+                      <MinusCircleIcon className="h-10" />
+                    </button>
+                    <span className="font-bold text-2xl">{quantity}</span>
+                    <button
+                      className=""
+                      onClick={() => {
+                        setQuantity(quantity + 1);
+                      }}
+                    >
+                      <PlusCircleIcon className="h-10" />
+                    </button>
+                  </div>
+
+                  <button
+                    disabled={cart.find((item) => item.id === product.id)}
+                    className="block px-5 py-3 sm:ml-3 text-xs font-medium text-white bg-fondo-300 rounded hover:bg-fondo-500 active:scale-75 transition-all ease-out disabled:pointer-events-none disabled:bg-zinc-400"
+                    onClick={addItemToCart}
+                  >
                     Add to cart
                   </button>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
