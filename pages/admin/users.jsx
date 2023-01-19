410433de9e26592f30400839a91fd590f1e2b71d
Bravoezz <joelmrd1234@gmail.com>
2023-01-19 14:20:10 -0500

    add transactions

--- pages/admin/users.jsx
+++ pages/admin/users.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import AdminLayout from "../../components/Elements_Admin/AdminLayout";
+import { useGetUserListQuery } from "../../redux/userApi";
 import adminMiddleware from "../../utils/adminMiddleware";
 
 const index = () => {
+  const {data , isLoading} = useGetUserListQuery()
+console.log(data)
   return (
     <AdminLayout>
       <h1>Users</h1>
