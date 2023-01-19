7eef6948a7d9ca1734da631e208bbd27eeb70a52
Your Name <you@example.com>
2023-01-19 19:33:48 -0300

    mod(pages/order-detail)

--- pages/order-detail.jsx
+++ pages/order-detail.jsx
@@ -31,7 +31,7 @@ const OrderDetail = () => {
       });
       const data = await response.json();
       //toast.loading("Redirecting...");
-      const hh = await stripe.redirectToCheckout({
+      await stripe.redirectToCheckout({
         sessionId: data.id,
       });
     } catch (error) {
@@ -41,12 +41,35 @@ const OrderDetail = () => {
 
   return (
     <Layout title="Lazy Buy | Order detail">
-      <h2 className="main text-fondo-400 font-bold text-2xl">Your order</h2>
-      <div className="main flex flex-wrap gap-2 gap-y-3 py-5 place-items-center " >
+      <h2 className="w-[95%] max-w-[1000px] mx-auto text-fondo-400 font-bold text-2xl mt-10 mb-8">Your order</h2>
+      <div className="flex sm:hidden w-[95%] max-w-[1000px] mx-auto min-h-[300px] rounded-md bg-zinc-100 shadow-md shadow-zinc-400 py-8 px-4 flex-col justify-between mb-16">
+          <div>
+            <h2 className="text-lg font-medium text-gray-600">Products:
+              <span className="ml-2 text-slate-500">
+                {cart.reduce((acc, item) => acc + item.quantity, 0)}
+              </span>
+            </h2>
+            <h2 className="text-lg font-medium text-gray-600">Summary:
+              <span className="ml-2 text-slate-500">
+                ${cart.reduce((acc, item) => acc + item.price, 0).toFixed(2)}
+              </span>
+            </h2>
+          </div>
+
+          <button
+              onClick={handlePayment}
+              className="py-3 px-4 bg-fondo-400 text-zinc-100 rounded-md"
+            >
+              Go checkout
+            </button>
+      </div>
+      
+      <div className="hidden sm:grid grid_order mb-16">
+        <div className="grid_products_order max-h-[400px] overflow-y-scroll">
         {cart.map((item) => (
           <div
             key={item.id}
-            className="relative bg-zinc-50 rounded-lg w-[250px] sm:w-[320px] md:w-[300px] shadow-md shadow-zinc-400 w-1/2"
+            className="bg-zinc-50 rounded-lg w-[160px] shadow-md shadow-zinc-400"
           >
             <div className="px-3 py-2 text-fondo-300">
               <h2 className=" overflow-hidden whitespace-nowrap text-ellipsis font-bold w-3/4">
@@ -73,32 +96,89 @@ const OrderDetail = () => {
             </div>
           </div>
         ))}
-    <div className="bg-zinc-200 rounded-lg fixed right-0 py-4 h-55 shadow-md shadow-gray-500 border border-slate-300 mr-4 gap-2 p-5">
-<div className="flex items-center ">
-  <div className="text-lg font-medium">Products:</div>
-  <div className="text-lg font-medium ml-2"> 
-            {cart
-              .reduce((acc, item) => acc + item.quantity, 0)
-              }</div>
-</div>
-<div className="my-4">
-  <span className="text-gray-600">Summary:</span>
-  <span className="text-lg font-medium ml-2">   $
-            {cart
-              .reduce((acc, item) => acc + item.price, 0)
-              .toFixed(2)}</span>
-</div>
-<div className="main flex ">
-        <button
-          onClick={handlePayment}
-          className="py-3 px-4 bg-fondo-400 text-zinc-100 rounded-md"
-        >
-          Go checkout
-        </button>
-      </div>
-</div>            
+        </div>
+      <div className="flex rounded-md bg-zinc-100 shadow-md max-h-[400px] shadow-zinc-400 py-8 px-4 flex-col justify-between">
+          <div>
+            <h2 className="text-lg font-medium text-gray-600">Products:
+              <span className="ml-2 text-slate-500">
+                {cart.reduce((acc, item) => acc + item.quantity, 0)}
+              </span>
+            </h2>
+            <h2 className="text-lg font-medium text-gray-600">Summary:
+              <span className="ml-2 text-slate-500">
+                ${cart.reduce((acc, item) => acc + item.price, 0).toFixed(2)}
+              </span>
+            </h2>
+          </div>
 
+          <button
+              onClick={handlePayment}
+              className="py-3 px-4 bg-fondo-400 text-zinc-100 rounded-md"
+            >
+              Go checkout
+            </button>
       </div>
+      </div>
+      {/* <div className="max-w-[1000px] mx-auto grid_order md:pl-4">
+        <div className="hidden md:grid grid_products_order">
+        {cart.map((item) => (
+          <div
+            key={item.id}
+            className="bg-zinc-50 rounded-lg w-[160px] shadow-md shadow-zinc-400"
+          >
+            <div className="px-3 py-2 text-fondo-300">
+              <h2 className=" overflow-hidden whitespace-nowrap text-ellipsis font-bold w-3/4">
+                {item.name}
+              </h2>
+            </div>
+            <Image
+              alt={item.name}
+              title={item.name}
+              src={item.mainImage.url}
+              width={320}
+              height={100}
+              className="w-[160px] h-[110px] object-cover"
+            />
+            <div className="bg-zinc-600 py-3 px-5 text-zinc-100 text-sm rounded-b-lg">
+              <p className="flex font-bold">
+                <ImPriceTag className="mr-1 mt-[5px]" />
+                {item.price.toFixed(2)}
+              </p>
+              <p className="flex font-bold">
+                <MdInventory className="mr-1 mt-[5px]" />
+                {item.quantity}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="bg-zinc-200 rounded-lg right-0 py-4 h-55 shadow-md shadow-gray-500 border border-slate-300 gap-2 p-5 max-sm:w-[90%] min-h-[400px] flex flex-col justify-between">
+          <div>
+            <div className="flex items-center">
+              <h2 className="text-lg font-medium text-gray-600">Products:</h2>
+              <span className="text-lg font-medium ml-2">
+                {cart.reduce((acc, item) => acc + item.quantity, 0)}
+              </span>
+            </div>
+            <div className="my-4">
+              <span className="text-gray-600">Summary:</span>
+              <span className="text-lg font-medium ml-2">
+                {" "}
+                ${cart.reduce((acc, item) => acc + item.price, 0).toFixed(2)}
+              </span>
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <button
+              onClick={handlePayment}
+              className="py-3 px-4 bg-fondo-400 text-zinc-100 rounded-md"
+            >
+              Go checkout
+            </button>
+          </div>
+        </div>
+      </div> */}
+
       {/* <div className="flex justify-center px-4 pb-3 mb-3 border-b">
           <p className="text-fondo-200 font-bold pr-1">Summary:</p>
           <div>
@@ -110,14 +190,15 @@ const OrderDetail = () => {
           </span>
           </div>
         </div> */}
+
     </Layout>
   );
 };
 
 export default OrderDetail;
 
-
-{/* <div className="bg-white p-6 rounded-lg">
+{
+  /* <div className="bg-white p-6 rounded-lg">
 <div className="flex items-center justify-between">
   <div className="text-lg font-medium">Products</div>
   <div className="text-lg font-medium">   $
@@ -132,4 +213,5 @@ export default OrderDetail;
               .reduce((acc, item) => acc + item.price * item.quantity, 0)
               .toFixed(2)}</span>
 </div>
-</div> */}
+</div> */
+}
