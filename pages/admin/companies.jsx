f69ed9ebbc065324f9ec3881a8a5092da0710460
Sojo506 <fabiansojowork@gmail.com>
2023-01-19 13:08:01 -0600

    feat(admin/companies): redux, component, table

--- pages/admin/companies.jsx
+++ pages/admin/companies.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import AdminLayout from "../../components/Elements_Admin/AdminLayout";
+import { Companies } from "../../components/Elements_Admin/Companies";
 import adminMiddleware from "../../utils/adminMiddleware";
 
 const index = ({ user }) => {
   return (
     <AdminLayout>
-      <h1>Companies</h1>
+      <h1 className="text-3xl font-bold tracking-wider text-fondo-300 capitalize text-center mb-10">
+        Companies Information
+      </h1>
+
+      <div className="overflow-auto rounded-lg shadow md:block">
+        <Companies />
+      </div>
     </AdminLayout>
   );
 };
