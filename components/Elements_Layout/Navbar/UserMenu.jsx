88522c3ef5cafe803ddbaa4d96d98f9670126379
AgDelfino <agustin.delfino92@gmail.com>
2023-01-20 18:30:32 -0300

    mod(Navbar/UserMenu.jsx): changed to modal type

--- components/Elements_Layout/Navbar/UserMenu.jsx
+++ components/Elements_Layout/Navbar/UserMenu.jsx
@@ -1,13 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { signOut } from "next-auth/react";
 import { clearSession } from "../../../redux/accountSlice";
-import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import { FiUser, FiHeart } from "react-icons/fi";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 
-const UserMenu = ({ active, setActive }) => {
+
+const backdrop = {
+  visible: {
+    opacity: 1,
+    transition:{ duration: 0.15}
+  },
+  hidden: {
+    opacity: 0,
+    transition:{ duration: 0.15}
+  },
+};
+
+const UserMenu = ({ showModal, setShowModal }) => {
   const dispatch = useDispatch();
   const session = useSelector((state) => state.account.session);
 
@@ -18,47 +29,63 @@ const UserMenu = ({ active, setActive }) => {
     });
   };
   const body = document.querySelector("main");
-  useEffect(() => {
-    body.onclick = () => setActive(false);
-    return () => {
-      body.onclick = null;
-    };
-  });
-  return (
-    <motion.div
-      initial={{ opacity: 0, height: 0 }}
-      animate={{ opacity: 1, height: "auto" }}
-      exit={{ opacity: 0, height: 0 }}
-      transition={{ duration: 0.1 }}
-      className="user_menu_pos top-[70px] w-[200px] rounded-md bg-fondo-200 text-zinc-100 flex flex-col gap-y-1 overflow-hidden"
-    >
-      {session.type === "user" && (
-        <>
-          <Link
-            href="/profile"
-            className="hover:bg-fondo-300 w-full px-2 py-2 text-left transition-colors duration-75 flex items-center"
-          >
-            <FiUser className="mr-2" />
-            Profile
-          </Link>
-          <Link
-            href="/profile/like"
-            className="hover:bg-fondo-300 w-full px-2 py-2 text-left transition-colors duration-75 flex items-center"
-          >
-            <FiHeart className="mr-2" />
-            Wishlist
-          </Link>
-        </>
-      )}
+  // useEffect(() => {
+  //   body.onclick = () => setActive(false);
+  //   return () => {
+  //     body.onclick = null;
+  //   };
+  // });
 
-      <button
-        onClick={handleSignOut}
-        className="hover:bg-fondo-300 w-full px-2 py-2 text-left transition-colors duration-75 flex items-center"
-      >
-        <RiLogoutBoxRLine className="mr-2" />
-        Sign out
-      </button>
-    </motion.div>
+  return (
+    <AnimatePresence>
+      {showModal && (
+        <motion.div
+          variants={backdrop}
+          className="fixed w-full h-full bg-[#01010152]  top-0 left-0 z-10"
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+        
+          <motion.div className="relative w-[90%] md:w-[50%] mt-24 h-60 mx-auto bg-fondo-300 flex flex-col items-center justify-evenly rounded-2xl">
+            <button
+              onClick={() => setShowModal((state) => !state)}
+              className="absolute top-4 right-4 p-1 bg-white rounded-full w-8"
+            >
+              X
+            </button>
+            <h2 className="text-2xl font-semibold text-white">
+              Already leaving?
+            </h2>
+            {session.type === "user" && (
+            <>
+              <Link
+                href="/profile"
+                className="hover:bg-fondo-300 w-full px-2 py-2 text-left transition-colors duration-75 flex items-center"
+              >
+                <FiUser className="mr-2" />
+                Profile
+              </Link>
+              <Link
+                href="/profile/like"
+                className="hover:bg-fondo-300 w-full px-2 py-2 text-left transition-colors duration-75 flex items-center"
+              >
+                <FiHeart className="mr-2" />
+                Wishlist
+              </Link>
+            </>
+          )}
+            <button
+              onClick={handleSignOut}
+              className="flex items-center bg-fondo-50 p-4 rounded-lg"
+            >
+              <RiLogoutBoxRLine className="mr-2" />
+              Sign out
+            </button>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
