0363402259d4f5c80c15d9b0f1d955192f021075
AgDelfino <agustin.delfino92@gmail.com>
2023-01-23 03:24:32 -0300

    deploy syntax error fixed

--- components/Elements_Dashboard/ModalMenu.jsx
+++ components/Elements_Dashboard/ModalMenu.jsx
@@ -19,10 +19,10 @@ const ModalMenu = ({ showModal, setShowModal }) => {
           initial="hidden"
           animate="visible"
           exit="hidden"
-          className="bg-fondo-300 w-[90%] gap-2 left-5 rounded-lg absolute mt-8 h-[100px] flex flex-col items-center justify-center text-white font-semibold py-20 over md:hidden"
+          className="bg-slate-700 shadow-md w-[90%] gap-2 left-5 rounded-xl absolute mt-8 h-[100px] flex flex-col items-center justify-center text-white font-semibold py-20 over md:hidden"
 
         >
-          <Link href="/dashboard/faqs">FAQ's</Link>
+          <Link href="/dashboard/faqs">FAQ&apos;s</Link>
           <Link href="/dashboard/faqs">Support</Link>
           <Link href="/dashboard/faqs">Plans</Link>
           {route.pathname !== "/dashboard" && (
