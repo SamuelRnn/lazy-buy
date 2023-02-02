e52085adbe6180aa6a5bae6a84293be00940bac0
Samuel Pérez <ricardo.reinn@outlook.com>
2023-02-02 14:24:24 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+const plugin = require("tailwindcss/plugin")
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
