d5ec363e7c9a047411036c9f3727170f8fae8657
SamuelRnn <ricardo.reinn@outlook.com>
2023-08-28 19:46:50 -0500

    tha fix

--- pages/api/get/company/[email].js
+++ pages/api/get/company/[email].js
@@ -1,67 +1,67 @@
-import { company } from "../../../../prisma";
-import simulateDelay from "../../../../utils/simulateDelay";
+import { company } from '../../../../prisma'
+import simulateDelay from '../../../../utils/simulateDelay'
 export default async function getCompanyById(req, res) {
-  if (req.method !== "GET")
-    return res.status(400).json({ message: "Not found" });
+	if (req.method !== 'GET') return res.status(400).json({ message: 'Not found' })
 
-  const all = req.query.email;
+	const all = req.query.email
 
-  const [email, page] = all.split("lazy");
-  const mm = all.includes("lazy");
+	const [email, page] = all.split('lazy')
+	const mm = all.includes('lazy')
 
-  if (!mm) {
-    const em = await company.findUnique({
-      where: { email: all },
-      include: {
-        products: {
-          where: {
-            isActive: true,
-          },
-          orderBy: {
-            updatedAt: "desc",
-          },
-        },
-        transactions: { include: { product: true } },
-      },
-    });
-    return res.status(200).json(em);
-  }
+	if (!mm) {
+		const em = await company.findUnique({
+			where: { email: all },
+			include: {
+				products: {
+					where: {
+						isActive: true,
+					},
+					orderBy: {
+						updatedAt: 'desc',
+					},
+				},
+				transactions: { include: { product: true } },
+			},
+		})
 
-  try {
-    const co = await company.findUnique({
-      where: { email },
-      include: {
-        products: {
-          where: {
-            isActive: true,
-          },
-        },
-      },
-    });
-    const companyF = await company.findUnique({
-      where: { email },
-      include: {
-        products: {
-          where: {
-            isActive: true,
-          },
-          orderBy: {
-            updatedAt: "desc",
-          },
-          skip: parseInt(page),
-          take: 10,
-        },
-        transactions: { include: { product: true } },
-      },
-    });
+		return res.status(200).json(em)
+	}
 
-    await simulateDelay(1);
-    if (!companyF) {
-      return res.status(404).json({ message: "Company not found, id invalid" });
-    }
-    return res.status(200).json({ co: co.products.length, companyF });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error });
-  }
+	try {
+		const co = await company.findUnique({
+			where: { email },
+			include: {
+				products: {
+					where: {
+						isActive: true,
+					},
+				},
+			},
+		})
+		const companyF = await company.findUnique({
+			where: { email },
+			include: {
+				products: {
+					where: {
+						isActive: true,
+					},
+					orderBy: {
+						updatedAt: 'desc',
+					},
+					skip: parseInt(page),
+					take: 10,
+				},
+				transactions: { include: { product: true } },
+			},
+		})
+
+		await simulateDelay(1)
+		if (!companyF) {
+			return res.status(404).json({ message: 'Company not found, id invalid' })
+		}
+		return res.status(200).json({ co: co.products.length, companyF })
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ error })
+	}
 }
--- pages/dashboard/products.jsx
+++ pages/dashboard/products.jsx
@@ -1,108 +1,112 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import DashboardLayout from "../../components/Elements_Dashboard/DashboardLayout";
-import CreationForm from "../../components/Elements_Dashboard/Products/CreationForm";
-import dashboardMiddleware from "../../utils/dashboardMiddleware";
-import { useEffect, useState } from "react";
-import { AnimatePresence } from "framer-motion";
+import DashboardLayout from '../../components/Elements_Dashboard/DashboardLayout'
+import CreationForm from '../../components/Elements_Dashboard/Products/CreationForm'
+import dashboardMiddleware from '../../utils/dashboardMiddleware'
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion'
 import {
-  useGetCompanyQuery,
-  useLazyGetCompanyPlanQuery,
-  useLazyGetProductQuery,
-} from "../../redux/companyApi";
-import Spinner from "../../components/Spinners/Spinner";
-import ProductCard from "../../components/Elements_Dashboard/Products/ProductCard";
-import PaginationAdminProduct from "../../components/Elements_Admin/PaginationAdminProduct";
+	useGetCompanyQuery,
+	useLazyGetCompanyPlanQuery,
+	useLazyGetProductQuery,
+} from '../../redux/companyApi'
+import Spinner from '../../components/Spinners/Spinner'
+import ProductCard from '../../components/Elements_Dashboard/Products/ProductCard'
+import PaginationAdminProduct from '../../components/Elements_Admin/PaginationAdminProduct'
 
 const Products = ({ company: { email } }) => {
-  const [active, setActive] = useState(false);
-  const [filt, setFilt] = useState({nd:"",pg:0})
-  const { isFetching,isLoading, data: company } = useGetCompanyQuery(email + "lazy" + filt.pg);
-  console.log(company)
-  const [fetchCompany, { data: plan }] = useLazyGetCompanyPlanQuery();
-  const [fetchProduct, { data: fetchedProduct }] = useLazyGetProductQuery();
-  const [displayedProduct, setDisplayedProduct] = useState(null);
+	const [active, setActive] = useState(false)
+	const [filt, setFilt] = useState({ nd: '', pg: 0 })
+	const { isFetching, isLoading, data: company } = useGetCompanyQuery(email + 'lazy' + filt.pg)
+	console.log(company)
+	const [fetchCompany, { data: plan }] = useLazyGetCompanyPlanQuery()
+	const [fetchProduct, { data: fetchedProduct }] = useLazyGetProductQuery()
+	const [displayedProduct, setDisplayedProduct] = useState(null)
 
-  useEffect(() => {
-    if (company && !plan) {
-      fetchCompany(company.companyF.plan);
-    }
-  }, [company]);
+	useEffect(() => {
+		if (company && !plan) {
+			fetchCompany(company.companyF.plan)
+		}
+	}, [company])
 
-  useEffect(() => {
-    if (fetchProduct) {
-      setDisplayedProduct(fetchedProduct);
-    }
-  }, [fetchedProduct]);
+	console.log(company)
 
-  const editProduct = async (id) => {
-    await fetchProduct(id);
-    setActive(true);
-    document.body.style.overflow = "hidden";
-  };
+	useEffect(() => {
+		if (fetchProduct) {
+			setDisplayedProduct(fetchedProduct)
+		}
+	}, [fetchedProduct])
 
-  return (
-    <DashboardLayout>
-      <AnimatePresence>
-        {active && (
-          <CreationForm
-            setActive={setActive}
-            product={displayedProduct}
-            companyId={company.companyF.id}
-            companyPlan={plan}
-          />
-        )}
-      </AnimatePresence>
-      <div className="relative">
-        <div className="flex justify-between items-center">
-          <h2 className="text-fondo-400 text-2xl font-bold">Your Products</h2>
-          <button
-            onClick={() => {
-              setActive(true);
-              setDisplayedProduct(null);
-              document.body.style.overflow = "hidden";
-            }}
-            className="py-3 bg-fondo-300 text-zinc-100 px-2 rounded-md"
-          >
-            Create Product
-          </button>
-        </div>
-        <br />
-        <div className="flex items-center justify-center">
-          {!isLoading && <PaginationAdminProduct setFilt={setFilt} page={company.co} pg={filt.pg}  />}
-        </div>
-        <br />
-        {/* products */}
-        <div className="pt-10 pb-40 relative">
-          {isFetching && (
-            <div className="flex justify-center absolute z-50 top-0 left-0 w-full min-h-screen bg-zinc-50 bg-opacity-60">
-              <div className="mt-48 flex items-center justify-center bg-zinc-700 rounded-md h-28 w-[300px]">
-                <p className="mr-4 text-zinc-100 text-lg">Loading products</p>
-                <Spinner />
-              </div>
-            </div>
-          )}
-          {!isLoading && (
-            <div className="grid_dashboard_products">
-              {[...company.companyF?.products]
-                .sort((a, b) => b.isVisible - a.isVisible)
-                .map((item) => (
-                  <ProductCard
-                    key={item.id}
-                    id={item.id}
-                    product={item}
-                    editProduct={editProduct}
-                  />
-                ))}
-            </div>
-          )}
-        </div>
-      </div>
-    </DashboardLayout>
-  );
-};
+	const editProduct = async (id) => {
+		await fetchProduct(id)
+		setActive(true)
+		document.body.style.overflow = 'hidden'
+	}
 
-export default Products;
+	return (
+		<DashboardLayout>
+			<AnimatePresence>
+				{active && (
+					<CreationForm
+						setActive={setActive}
+						product={displayedProduct}
+						companyId={company.companyF.id}
+						companyPlan={plan}
+					/>
+				)}
+			</AnimatePresence>
+			<div className='relative'>
+				<div className='flex justify-between items-center'>
+					<h2 className='text-fondo-400 text-2xl font-bold'>Your Products</h2>
+					<button
+						onClick={() => {
+							setActive(true)
+							setDisplayedProduct(null)
+							document.body.style.overflow = 'hidden'
+						}}
+						className='py-3 bg-fondo-300 text-zinc-100 px-2 rounded-md'
+					>
+						Create Product
+					</button>
+				</div>
+				<br />
+				<div className='flex items-center justify-center'>
+					{!isLoading && (
+						<PaginationAdminProduct setFilt={setFilt} page={company.co} pg={filt.pg} />
+					)}
+				</div>
+				<br />
+				{/* products */}
+				<div className='pt-10 pb-40 relative'>
+					{isFetching && (
+						<div className='flex justify-center absolute z-50 top-0 left-0 w-full min-h-screen bg-zinc-50 bg-opacity-60'>
+							<div className='mt-48 flex items-center justify-center bg-zinc-700 rounded-md h-28 w-[300px]'>
+								<p className='mr-4 text-zinc-100 text-lg'>Loading products</p>
+								<Spinner />
+							</div>
+						</div>
+					)}
+					{!isLoading && (
+						<div className='grid_dashboard_products'>
+							{[...company.companyF?.products]
+								.sort((a, b) => b.isVisible - a.isVisible)
+								.map((item) => (
+									<ProductCard
+										key={item.id}
+										id={item.id}
+										product={item}
+										editProduct={editProduct}
+									/>
+								))}
+						</div>
+					)}
+				</div>
+			</div>
+		</DashboardLayout>
+	)
+}
+
+export default Products
 
 export async function getServerSideProps(context) {
-  return await dashboardMiddleware(context);
+	return await dashboardMiddleware(context)
 }
